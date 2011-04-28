bebfea3f92068da40bc64f7ebdb4ccb0bea58b20
LorD ClockaN <davor@losinj.com>
2011-04-28 16:07:36 +0200

    Initial commit for updates lordmodhd_defconfig created for CM7 trash .h files

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Wed Apr 27 21:34:21 CEST 2011"
+#define LINUX_COMPILE_TIME "21:34:21"
+#define LINUX_COMPILE_BY "lord"
+#define LINUX_COMPILE_HOST "lord-mint"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "

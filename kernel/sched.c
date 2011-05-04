46ccca6068494fc3dbc1a2cac9db693c4a7275fb
LorD ClockaN <davor@losinj.com>
2011-05-04 16:46:31 +0200

    ADD: reverted and fixed some patches

--- kernel/sched.c
+++ kernel/sched.c
@@ -8370,7 +8370,7 @@ void sched_move_task(struct task_struct *tsk)
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	if (tsk->sched_class->task_move_group)
-		tsk->sched_class->moved_group(tsk, on_rq);
+		tsk->sched_class->task_move_group(tsk, on_rq);
 	else
 #endif
 		set_task_rq(tsk, task_cpu(tsk));
